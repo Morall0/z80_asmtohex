0fbe79651c4cb379ad764b752043c393cf7a9df1
Morall0 <mau_ml@comunidad.unam.mx>
2024-11-15 22:18:39 -0600

    ADD: comentarios a lut.py

--- lut.py
+++ lut.py
@@ -6,6 +6,8 @@ lut = {}  # Look Up Table con las instrucciones y sus codigos en hexadecimal
 with open("TablaZ80.csv", "r") as archivo_csv:
     lector = csv.reader(archivo_csv, delimiter=";")
     for fila in lector:  # Llenado de la LUT con el contenido de cada fila
-        key = fila[0]
-        values = fila[1:]
+        key = fila[0] # Toma la instruccion
+        values = fila[1:] # Toma el código y el hex
         lut[key] = values
+
+print(lut["LD A, (NN)"])
