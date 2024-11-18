d5755269bc7ed86bccab092903c875e584c000e2
Morall0 <mau_ml@comunidad.unam.mx>
2024-11-18 16:26:46 -0600

    MOD: se cambia el nombre de las variables en la creacion de la lut

--- lut.py
+++ lut.py
@@ -6,8 +6,6 @@ lut = {}  # Look Up Table con las instrucciones y sus codigos en hexadecimal
 with open("TablaZ80.csv", "r") as archivo_csv:
     lector = csv.reader(archivo_csv, delimiter=";")
     for fila in lector:  # Llenado de la LUT con el contenido de cada fila
-        key = fila[0] # Toma la instruccion
-        values = fila[1:] # Toma el código y el hex
-        lut[key] = values
-
-print(lut["LD A, (NN)"])
+        instruccion = fila[0]  # Toma la instruccion
+        cod, tam = fila[1:]  # Toma el código en hex y el tamaño
+        lut[instruccion] = [cod, tam]
