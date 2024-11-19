405c7e6be43eca8b24656f333eced11ce246fa59
Morall0 <mau_ml@comunidad.unam.mx>
2024-11-18 21:09:07 -0600

    ADD: archivo .asm de pruebas

---
+++ reloc.asm
@@ -0,0 +1,51 @@
+      LD BC, 127
+      LD A, 0; r <- 0
+      LD (1000H), A 
+      
+      ; RELOCALIZACION CON ESPACIOS
+      LD B, 0; BC <- n (tamamaño del bloaque)
+      LD A, (1001H)
+      LD C, A
+      LD HL, 1001H; HL <- o[n-1]
+      ADD HL, BC
+      LD DE, 7FFFH; DE <- d[m-1] de la memoria
+
+      LDDR
+      INC DE; DE <- d[0]
+
+      ; BUSQUEDA DE LOS ESPACIOS
+      LD A, (1001H); BC <- n (tamaño del bloque) ; TODO CAMBIAR EL TAMANO DEL BLOQUE
+      LD C, A
+eti2:
+      LD H, D; HL <- DE
+      LD L, E;
+      LD A, 0; A <- 0
+      CPIR
+      LD A, C
+      CP 0
+      JP Z, eti1;
+      DEC HL
+      
+      ; ELIMINACION DEL ESPACIO (RECORRIDO) 
+      LD A, L
+      SUB E
+      LD C, A
+      EX DE, HL
+      ADD HL, BC
+      DEC L
+      ;LD A, 0 No es necesario cargar A con 0
+      LDDR
+      INC DE; DE <- d[0]
+      INC HL
+      LD A, E; Encontrando cantidad de espacios
+      SUB L
+      LD H, A; Respaldando cantidad de espacios quitados en H
+      LD A, (1001H); A <- n
+      SUB H; Restandole al tamaño los espacios quitados
+      LD (1001H), A; Cambiando en la memoria el tamaño del bloque
+      LD C, A; BC <- nuevo n
+      JP eti2 
+eti1:
+      HALT
+
+eti0: LD A, (1000)
--- traductor.py
+++ traductor.py
@@ -1,5 +1,5 @@
 from lut import lut
-from dec_to_hex import convert_dtoh
+# from dec_to_hex import convert_dtoh
 import re
 
 
@@ -18,6 +18,8 @@ def hay_instruccion(linea: str) -> bool:
     return no_linea_vacia and no_linea_coment
 
 
+# TODO: Funcion que añada ceros
+
 # Lectura del archivo
 with open("reloc.asm", "r") as archivo:
     linea = archivo.readline()
@@ -26,18 +28,61 @@ with open("reloc.asm", "r") as archivo:
             if linea.find(":") != -1:  # Verifica si hay eti
                 linea = re.split(r":", linea)
                 eti = linea[0]
+                instruccion = ""
                 if linea[1][0] != "\n":  # Verif si hay instr después de la eti
                     instruccion = linea[1]
                     instruccion = limpa_instruccion(instruccion)
-                    # print(instruccion)
                 # TODO: Manejar las referencias (etis)
             else:
                 instruccion = limpa_instruccion(linea)
 
+            if instruccion != "":
+
+                # Separacion de las instrucciones
+                div_inst = instruccion.split(" ")
+                instruccion = div_inst[0]
+                num_operandos = len(div_inst[1:])
+
+                if num_operandos == 0:
+                    print(instruccion)
+                    print(lut[instruccion])
+
+                elif num_operandos == 1:
+                    op1 = div_inst[1]
+
+                    # Registros
+                    if re.match(r"[A-EHL]$", op1):
+                        print(instruccion+" "+op1)
+                        print(lut[instruccion+" "+op1])
+
+                    # Registros pares
+                    elif re.match(r"(AF|BC|DE|HL|SP|IX|IY)$", op1):
+                        print(instruccion+" "+op1)
+                        print(lut[instruccion+" "+op1])
+
+                    # (HL) | (IX) | (IY)
+                    elif re.match(r"\((HL|IX|IY)\)", op1):
+                        print(lut[instruccion+" "+op1])
+
+                    # N o NN
+                    elif re.match(r"[0-9A-F]{1,4}H", op1):
+                        if re.match(r"(JP|CALL)", instruccion):
+                            print(lut[instruccion+" NN"])
+                        else:
+                            print(lut[instruccion+" N"])
+
+                    # (IX+D) | (IY+D)
+                    elif re.match(r"\((IX|IY)\+[0-9AF]{1,2}H\)", op1):
+                        print(lut[instruccion+" D"])
+
+                elif num_operandos == 2:
+                    op1 = div_inst[1]
+                    op2 = div_inst[2]
+                    # print(instruccion, op1, op2)
+
             # Reemplaza los numeros en decimal, por hexa
-            numero = re.search(r"\b\d+(?!H)\b", instruccion)
-            if numero is not None:
-                instruccion = re.sub(r"\b\d+(?!H)\b",  convert_dtoh(numero.group(0))+"H", instruccion)
-            print(instruccion)
+            # numero = re.search(r"\b\d+(?!H)\b", instruccion)
+            # if numero is not None:
+            #     instruccion = re.sub(r"\b\d+(?!H)\b",  convert_dtoh(numero.group(0))+"H", instruccion)
 
         linea = archivo.readline()
